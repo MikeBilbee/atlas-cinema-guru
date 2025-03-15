009c526222c0ad1f0ad56b886d73afae47298335
MikeBilbee <cmbilbee@gmail.com>
2025-03-15 11:53:29 -0500

    3.0 Favorites and page updating

---
+++ app/favorites/page.tsx
@@ -0,0 +1,79 @@
+// app/favorites/page.tsx
+"use client"
+
+import { useState, useEffect } from "react";
+import MovieTile from "@/components/MovieTile";
+import PageButtons from "@/components/PageButtons";
+
+interface Film {
+	title: string;
+	id: string;
+	released: string;
+	synopsis: string;
+	genre: string;
+	favorited: boolean;
+	watchLater: boolean;
+  }
+  
+  const Favorites: React.FC = () => {
+	const [films, setFilms] = useState<Film[]>([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const filmsPerPage = 6;
+	const [refreshKey, setRefreshKey] = useState(0);
+  
+	useEffect(() => {
+	  const fetchFavorites = async () => {
+		try {
+		  const response = await fetch(`/api/favorites?page=${currentPage}`);
+		  if (!response.ok) {
+			throw new Error(`API request failed with status ${response.status}`);
+		  }
+		  const data = await response.json();
+		  setFilms(data.favorites);
+		} catch (error) {
+		  console.error("Error fetching favorites:", error);
+		}
+	  };
+  
+	  fetchFavorites();
+	}, [currentPage, refreshKey]);
+  
+	const handleFavoriteToggle = () => {
+	  setRefreshKey(refreshKey + 1);
+	};
+  
+	const handleWatchLaterToggle = () => {
+	  setRefreshKey((prevKey) => prevKey + 1);
+	};
+  
+	const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  
+	return (
+	  <div key={refreshKey} className="mt-8">
+		  <h2 className="text-center text-4xl font-bold">Favorites</h2>
+		<div className="grid grid-cols-3 px-6 gap-16 mt-8">
+		  {films.map((film) => (
+			<MovieTile
+			  key={film.id}
+			  title={film.title}
+			  coverArtUrl={`/images/${film.id}.webp`}
+			  released={film.released}
+			  synopsis={film.synopsis}
+			  genre={film.genre}
+			  id={film.id}
+			  onFavoriteToggle={handleFavoriteToggle}
+			  onWatchLaterToggle={handleWatchLaterToggle}
+			/>
+		  ))}
+		</div>
+		<PageButtons
+		  totalItems={100} // Replace with actual total favorites count
+		  itemsPerPage={filmsPerPage}
+		  currentPage={currentPage}
+		  onPageChange={paginate}
+		/>
+	  </div>
+	);
+  };
+  
+  export default Favorites;
